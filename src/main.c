65b611af13aee6d4d940f48151ca38e3519575b9
Bryant Pong <rcxking@gmail.com>
2016-08-28 23:23:29 -0700

    Adding Makefile and main.c

---
+++ src/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ * This is the maze runner program for the Homebrew Robotics Club
+ * Maze Challenge.
+ *
+ * This code is designed to run on a Pololu 3pi robot.
+ *
+ * Bryant Pong
+ * 8/28/16
+ */
+
+// 3rd Party Libraries:
+#include <pololu/3pi.h> // 3 pi library
+
+// Main function:
+int main()
+{
+	return 0;
+}
