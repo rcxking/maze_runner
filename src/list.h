c96197e9c7c825ac369b5c91f6bae607a6610797
Bryant Pong <rcxking@gmail.com>
2016-09-26 23:44:33 -0700

    Implemented list's assignment operator= and the push_back function.

--- src/list.h
+++ src/list.h
@@ -8,7 +8,7 @@
  * Bryant Pong
  * 9/4/16
  *
- * Last Updated: 9/25/16 
+ * Last Updated: 9/26/16 
  */
 #ifndef _LIST_H_
 #define _LIST_H_
@@ -136,27 +136,93 @@ public:
 	~List() { this->deleteList(); }
 	List& operator=( const List& rhs );
 
+	// Utility Functions:
+	inline unsigned int size() const { return size_; }
+	inline bool empty() const { return ( head_ == NULL ); }
+	void clear() { this->deleteList(); }
+
+	// Adding/Removing Elements:
 	void push_front( const T& obj );
+	void pop_front();
 	void push_back( const T& obj ); 
+	void pop_back();
+	
+	// Iterator Support:
+	typedef ListIterator<T> iterator;
+	iterator erase( iterator itr );
+	iterator insert( iterator itr, T const& value );
+	inline iterator begin() { return iterator( head_ ); }
+	inline iterator end() { return iterator( NULL ); }
 
 private:
 	// Helper functions to copy and delete the list safely:
-	void copyList();
+	void copyList( const List<T>& rhs );
 	void deleteList(); 
-
-	// Length of this list:
-	unsigned int size_;
 
 	// Pointer to the head node:
 	Node< T > *head_; 
 	// Pointer to the tail node:
 	Node< T > *tail_; 
+
+	// Length of this list:
+	unsigned int size_;
 };
+
+// Implementation of assignment operator=:
+template <class T> List<T>& List<T>::operator=( const List<T>& rhs )
+{
+	if( &rhs != this )
+	{
+		this->deleteList();
+		this->copyList();
+	}
+
+	return *this;
+}
+
+// Push Back/Push Front:
+template <class T> void List<T>::push_back( const T& value )
+{
+	// Check if the head is NULL:
+	if( head_ == NULL )
+	{
+		// Create a new node and make that the head:  
+		Node<T> *n = new Node<T>;
+		n->value = value;
+		n->next = NULL;
+		n->prev = NULL;
+		head_ = n;
+		tail_ = n;
+		size_++;
+	}
+	else
+	{
+		// Walk through the existing list and append the new node at the end:
+		Node<T> *head = head_;
+		for( ; head->next != NULL; head = head->next );
+
+		// Create the new node:
+		Node<T> *n = new Node<T>;
+		n->value = value;
+		n->next = NULL;
+		n->prev = head;
+		
+		head->next = n;	
+		tail_ = n;
+		size_++; 
+	}
+}
+
+// Function to go through the linked list and copy all objects:
+template <class T> void List<T>::copyList( const List<T>& rhs )
+{
+}
 
 // Function to go through the linked list and delete all objects safely:
 template <class T> void List<T>::deleteList()
 {
 	
 }
+     
 
 #endif
--- test_programs/listtest.cpp
+++ test_programs/listtest.cpp
@@ -6,7 +6,7 @@
  * Bryant Pong
  * 9/25/16
  *
- * Last Updated: 9/25/16   
+ * Last Updated: 9/26/16   
  */
 
 // Custom Includes:
@@ -18,8 +18,19 @@ int main()
 {
 	// Create an integer linked list:
 	List<int> intList; 
+	intList.push_back( 1 );
+	intList.push_back( 2 ); 	
+	intList.push_back( 3 );
+	intList.push_back( 4 );
+	intList.push_back( 5 );
 
-		
+	// Iterate through the list and print out the results:
+	std::cout << "Now printing out intList" << std::endl;
+	List<int>::iterator itr; 
+	for( itr = intList.begin(); itr != intList.end(); ++itr )
+	{
+		std::cout << *itr << std::endl;
+	}
 
 	return 0;
 }
